6b741e563668befd9d03b996c8826207e6103289
Andrea Raiola <a.raiola14@studenti.unisa.it>
2023-02-19 22:28:06 +0100

    Pretest

--- src/main/java/net/javaguides/springboot/service/UserServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/UserServiceImpl.java
@@ -39,18 +39,5 @@ public class UserServiceImpl implements UserService{
 		return userRepository.save(user);
 	}
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	
-		User user = userRepository.findByEmail(username);
-		if(user == null) {
-			throw new UsernameNotFoundException("Invalid username or password.");
-		}
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));		
-	}
-	
-	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
-		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-	}
-	
+
 }
